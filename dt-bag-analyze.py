7990ae9c26875caa9cedba4dbb5a6acc2db34363
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-10-10 12:06:44 +0200

    updates

--- dt-bag-analyze.py
+++ dt-bag-analyze.py
@@ -1,6 +1,38 @@
 import rosbag
 import sys
+import numpy as np
 
-fh = sys.argv[1]
+bag_file = sys.argv[1]
 
+bag = rosbag.Bag(bag_file)
+
+nmsg = {}
+tlst = {}
+
+def to_interval(lst):
+	ilst = []
+	for j in range(0, len(lst) - 1):
+		ilst.append(lst[j+1] - lst[j])
+	return ilst
+
+for topic, msg, t in bag.read_messages():
+
+	if not (topic in nmsg):
+		nmsg[topic] = 0
+		tlst[topic] = []
+
+	nmsg[topic] += 1
+	tlst[topic].append(t.to_sec())
+
+for topic in tlst:
+	iv = np.array(to_interval(tlst[topic]))
+	print topic + ":"
+	print "\tnum_messages:", nmsg[topic]
+	print "\tperiod:"
+	print "\t\tmin:", iv.min()
+	print "\t\tmax:",iv.max()
+	print "\t\taverage:",iv.mean()
+	print "\t\tmedian:",np.median(iv)
+
+bag.close()
 
